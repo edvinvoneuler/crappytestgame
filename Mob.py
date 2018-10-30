e2c71c8568e528abb9d82fac86796a96156bacf6
edvinvoneuler <39345445+edvinvoneuler@users.noreply.github.com>
2018-10-30 22:48:02 +0100

    Update Mob.py

---
+++ Mob.py
@@ -0,0 +1,10 @@
+import Weapon as We
+
+
+class Mob:
+
+    def __init__(self, name):
+        self.name = name
+        if name == "Kobold":
+            self. weapon = We.Weapon("Kobold Spear")
+            self.hp = 38
